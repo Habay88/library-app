0a8cc9ace112754f826a880d9430188e30d3eafa
Habay <biodun.banjoko@yahoo.com>
2023-05-10 18:32:44 +0100

    Searchbook page pagination

--- src/searchbookspage/SearchBooksPage.tsx
+++ src/searchbookspage/SearchBooksPage.tsx
@@ -3,21 +3,32 @@ import { useState, useEffect } from "react";
 import SpinnerLoading from "../utils/SpinnerLoading";
 import BookModel from "../models/BookModel";
 import SearchBook from "./component/SearchBook";
+import Pagination from "../utils/Pagination";
 
 const SearchBooksPage = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
   useEffect(() => {
     const fetchBooks = async () => {
       const baseUrl: string = "http://localhost:1988/api/books";
-      const url: string = `${baseUrl}?page=0&size=5`;
+      const url: string = `${baseUrl}?page=${
+        currentPage - 1
+      }&size=${booksPerPage}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Something went wrong !!");
       }
       const responseJson = await response.json();
       const responseData = responseJson._embedded.books;
+      setTotalAmountOfBooks(responseJson.page.totalElements);
+      setTotalPages(responseJson.page.totalElements);
+
       const loadedBooks: BookModel[] = [];
       for (const key in responseData) {
         loadedBooks.push({
@@ -38,7 +49,8 @@ const SearchBooksPage = () => {
       setIsLoading(false);
       setHttpError(error.message);
     });
-  }, []);
+    window.scrollTo(0, 0);
+  }, [currentPage]);
   if (isLoading) {
     return (
       <div className="container m-5">
@@ -53,64 +65,84 @@ const SearchBooksPage = () => {
       </div>
     );
   }
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
 
-  return <div>
-    <div className="container">
-      <div>
-        <div className="row mt-5">
-        <div className="col-6">
-          <div className="d-flex">
-            <input className='form-control me-2' type="search" placeholder="Search" aria-labelledby="Search"/>
-            <button className="btn btn-outline-success">
-                Search
-            </button>
-            
-          </div>
-
-        </div>
-        <div className='col-4'>
-            <div className="dropdown">
-            <button className='btn btn-secondary dropdown-toggle' type='button' id="dropdownMenuButton1" data-bs-toggle='dropdown' aria-expanded='false' >
-             Category
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li>
-                <a className="dropdown-item" href="#">
-                    All
-                </a>
-                <a className="dropdown-item" href="#">
-                    FrontEnd
-                </a>
-                <a className="dropdown-item" href="#">
-                    Backend
-                </a>
-                <a className="dropdown-item" href="#">
-                    Devops
-                </a>
-                <a className="dropdown-item" href="#">
-                    FullStack
-                </a>
-
-              </li>
-
-            </ul>
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <div className="row mt-5">
+            <div className="col-6">
+              <div className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-labelledby="Search"
+                />
+                <button className="btn btn-outline-success">Search</button>
+              </div>
             </div>
+            <div className="col-4">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Category
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      All
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      FrontEnd
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Backend
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Devops
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      FullStack
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="mt-3">
+            <h5>Number of results: ({totalAmountOfBooks})</h5>
+          </div>
+          <p>{indexOfFirstBook + 1} 0f {lastItem} of {totalAmountOfBooks} items:</p>
+          {books.map((book) => (
+            <SearchBook book={book} key={book.id} />
+          ))}
+          {totalPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              paginate={paginate}
+            />
+          )}
         </div>
-        </div>
-        <div className="mt-3">
-        <h5>Number of results: (22)</h5>
-        </div>
-        <p>
-          1 0f 5 of 22 items:
-        </p>
-        {books.map(book =>(
-          <SearchBook book={book} key={book.id} />
-        ))}
       </div>
-
     </div>
-
-          </div>;
+  );
 };
 
 export default SearchBooksPage;
