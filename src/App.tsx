8f8ff6865700e50a8ac0ca0fdef788a439ea1926
Habay <biodun.banjoko@yahoo.com>
2023-05-10 10:39:57 +0100

    search component with css error

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Navbar from './layouts/NavbarAndFooter/Navbar';
 
 import Footer from './layouts/NavbarAndFooter/Footer';
 import HomePage from './layouts/NavbarAndFooter/HomePage/HomePage';
+import SearchBooksPage from './searchbookspage/SearchBooksPage';
 
 export const App =()=> {
   return (
     <div>
   <Navbar/>
- <HomePage/>
+{/*  <HomePage/> */}
+<SearchBooksPage/>
   <Footer/>
   </div>
   );
