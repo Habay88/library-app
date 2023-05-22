6f2f77559070d356e46d615ff834b68fe135d625
Habay <biodun.banjoko@yahoo.com>
2023-05-22 18:24:53 +0100

    error checkout

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -5,7 +5,7 @@ import SpinnerLoading from "../../utils/SpinnerLoading";
 
 const BookCheckoutPage = () => {
 
-     const [book, setBook] = useState<BookModel>();
+    const [book, setBook] = useState<BookModel>();
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
@@ -16,9 +16,9 @@ const BookCheckoutPage = () => {
             const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
 
             const response = await fetch(baseUrl);
-
-            if (!response.ok) {
-                throw new Error('Something went wrong!');}
+if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
 
             const responseJson = await response.json();
 
@@ -59,33 +59,44 @@ const BookCheckoutPage = () => {
 
     return (
         <div>
-           <div className="container d-none d-lg-block">
-            <div className="row mt-5">
-                <div className="col-sm-2 col-md-2" >
-               {
-                book?.img?
-                <img src={book?.img} width='226' height='349' alt="Book"/>
-              :
-              <img src={ require('../../Images/BooksImages/book-luv2code-1000.png')} width='226'
-              height='349' alt='Book'/>
-            }
+            <div className='container d-none d-lg-block'>
+                <div className='row mt-5'>
+                    <div className='col-sm-2 col-md-2'>
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                                height='349' alt='Book' />
+                        }
+                    </div>
+                    <div className='col-4 col-md-4 container'>
+                        <div className='ml-2'>
+                            <h2>{book?.title}</h2>
+                            <h5 className='text-primary'>{book?.author}</h5>
+                            <p className='lead'>{book?.description}</p>
+                        </div>
+                    </div>
                 </div>
-<div className="col-4 col-md-4 container">
-    <div className="ml-2">
-        <h2>{book?.title}</h2>
-        <h5 className="text-primary">{book?.author}</h5>
-        <p className="lead">{book?.description}</p> 
-        </div>
-    </div>
-</div>
-<hr/>
+                <hr />
             </div>
-            <div className="container d-lg-none mt-5">
-                <div className="d-flex justify-content ">
-
+            <div className='container d-lg-none mt-5'>
+                <div className='d-flex justify-content-center align-items-center'>
+                    {book?.img ?
+                        <img src={book?.img} width='226' height='349' alt='Book' />
+                        :
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                            height='349' alt='Book' />
+                    }
                 </div>
-
-           </div>
+                <div className='mt-4'>
+                    <div className='ml-2'>
+                        <h2>{book?.title}</h2>
+                        <h5 className='text-primary'>{book?.author}</h5>
+                        <p className='lead'>{book?.description}</p>
+                    </div>
+                </div>
+                <hr />
+            </div>
         </div>
     );
 }
