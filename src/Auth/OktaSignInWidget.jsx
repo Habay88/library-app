be73779497c5a5f11f5bf45a625fd29eed0b820d
Habay <biodun.banjoko@yahoo.com>
2023-06-11 16:26:06 +0100

    possible update

--- src/Auth/OktaSignInWidget.jsx
+++ src/Auth/OktaSignInWidget.jsx
@@ -2,10 +2,12 @@ import {useEffect,useRef} from 'react';
 //import {OktaSignIn} from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 import { oktaConfig } from '../lib/oktaConfig';
-const OktaSignIn = require('@okta/okta-signin-widget/dist/js/okta-sign-in.min.js');
+// const OktaSignIn = require('@okta/okta-signin-widget/dist/js/okta-sign-in.min.js');
+ 
+import OktaSignIn from '@okta/okta-signin-widget';
 const OktaSignInWidget = ({onSuccess, onError}) => {
     const widgetRef = useRef();
-
+ 
     useEffect(() => {
         if(!widgetRef.current) {
             return false;
@@ -14,15 +16,15 @@ const OktaSignInWidget = ({onSuccess, onError}) => {
         widget.showSignInToGetTokens({
             el: widgetRef.current,
         }).then(onSuccess).catch(onError);
-        return () => widgetRef.remove();
+        return () => widget.remove();
     }, [onSuccess,onError]);
-
-
+ 
+ 
        return(
            <div className="container mt-5 mb-5">
            <div ref={widgetRef}> </div>
            </div> 
         );
     };
-
+ 
 export default OktaSignInWidget;
--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig ={
-    clientId: `0oa9vx2r0jROVhpo25d7`,
-    issuer: `https://dev-13033890.okta.com/oauth2/default`,
+    clientId: '0oa9vx2r0jROVhpo25d7',
+    issuer: 'https://dev-13033890.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes : ['openid', 'profile', 'email'],
     pkce: true,
